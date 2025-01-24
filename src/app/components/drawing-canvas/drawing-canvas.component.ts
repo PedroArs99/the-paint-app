b96f0ae3717ef1341ff0a79d0c2d3947ded2ddd9
Pedro Arenas <par@novatec-gmbh.de>
2025-01-24 17:01:56 +0100

    Refactor to introduce drawing service

--- src/app/components/drawing-canvas/drawing-canvas.component.ts
+++ src/app/components/drawing-canvas/drawing-canvas.component.ts
@@ -2,6 +2,7 @@ import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import Konva from 'konva';
 import { KonvaEventListener } from 'konva/lib/Node';
 import { Stage } from 'konva/lib/Stage';
+import { DrawingService } from '../../services/drawing.service';
 
 @Component({
   selector: 'app-drawing-canvas',
@@ -13,58 +14,12 @@ export class DrawingCanvasComponent implements AfterViewInit {
   @ViewChild('canvasContainer')
   canvasContainer?: ElementRef;
 
+  constructor(private drawingService: DrawingService) {}
+
   ngAfterViewInit(): void {
     const width = this.canvasContainer?.nativeElement.clientWidth;
     const height = this.canvasContainer?.nativeElement.clientHeight;
 
-    let stage = new Konva.Stage({
-      container: 'canvas-container',
-      width,
-      height
-    });
-
-    let layer = new Konva.Layer();
-    stage.add(layer);
-
-    let isPainting = false;
-    let lastLine: Konva.Line;
-
-    stage.on('mousedown touchstart', function (e) {
-      isPainting = true;
-      
-      let pos = stage.getPointerPosition();
-      if (!pos) return;
-
-      lastLine = new Konva.Line({
-        stroke: '#df4b26',
-        strokeWidth: 5,
-        globalCompositeOperation: 'source-over',
-        // round cap for smoother lines
-        lineCap: 'round',
-        lineJoin: 'round',
-        // add point twice, so we have some drawings even on a simple click
-        points: [pos.x, pos.y, pos.x, pos.y],
-      });
-      layer.add(lastLine);
-    });
-
-    stage.on('mouseup touchend', function () {
-      isPainting = false;
-    });
-
-    stage.on('mousemove touchmove', function (e) {
-      if (!isPainting) {
-        return;
-      }
-
-      // prevent scrolling on touch devices
-      e.evt.preventDefault();
-
-      const pos = stage.getPointerPosition();
-      if (!pos) return;
-
-      var newPoints = lastLine.points().concat([pos.x, pos.y]);
-      lastLine.points(newPoints);
-    });
+    this.drawingService.initStage('canvas-container', width, height);
   }
 }
---
+++ src/app/services/drawing.service.ts
@@ -0,0 +1,73 @@
+import { Injectable } from '@angular/core';
+import Konva from 'konva';
+import { KonvaEventObject } from 'konva/lib/Node';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DrawingService {
+  private stage?: Konva.Stage;
+  private drawingLayer?: Konva.Layer;
+
+  private isPainting = false;
+  private currentPainting?: Konva.Line;
+
+  initStage(containerId: string, width: number, height: number) {
+    this.stage = new Konva.Stage({
+      container: containerId,
+      width,
+      height,
+    });
+
+    this.drawingLayer = new Konva.Layer();
+    this.stage.add(this.drawingLayer);
+
+    this.stage.on('mousedown touchstart', this.onPaintingStart.bind(this));
+    this.stage.on('mousemove touchmove', (e) => this.onPaintingPointerMove.bind(this)(e));
+    this.stage.on('mouseup touchend', this.onPaintingEnd.bind(this));
+  }
+
+  private onPaintingStart() {
+    this.isPainting = true;
+
+    let pointerPosition = this.stage?.getPointerPosition();
+    if (!pointerPosition) return;
+
+    this.currentPainting = new Konva.Line({
+      stroke: '#3',
+      strokeWidth: 5,
+      globalCompositeOperation: 'source-over',
+      // round cap for smoother lines
+      lineCap: 'round',
+      lineJoin: 'round',
+      // add point twice, so we have some drawings even on a simple click
+      points: [
+        pointerPosition.x,
+        pointerPosition.y,
+        pointerPosition.x,
+        pointerPosition.y,
+      ],
+    });
+
+    this.drawingLayer?.add(this.currentPainting);
+  }
+
+  private onPaintingPointerMove(e: KonvaEventObject<any, Konva.Stage>) {
+    if (!this.isPainting) return;
+
+    // prevent scrolling on touch devices
+    e.evt.preventDefault();
+
+    let pointerPosition = this.stage?.getPointerPosition();
+    if (!pointerPosition) return;
+
+    var newPoints = this.currentPainting
+      ?.points()
+      .concat([pointerPosition.x, pointerPosition.y]);
+    this.currentPainting?.points(newPoints ?? []);
+  }
+
+  private onPaintingEnd() {
+    this.isPainting = false;
+  }
+}
